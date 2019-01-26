1e545bcb478d6b128fed7ba1d4b2368130e2dc3a
Joe Greubel <joegreubel1@gmail.com>
2019-01-26 14:21:38 -0800

    Initial commit

---
+++ auth-provider/src/main/java/com/jgreubel/oauthauthprovider/OauthAuthProviderApplication.java
@@ -0,0 +1,14 @@
+package com.jgreubel.oauthauthprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAuthProviderApplication.class, args);
+	}
+
+}
+
