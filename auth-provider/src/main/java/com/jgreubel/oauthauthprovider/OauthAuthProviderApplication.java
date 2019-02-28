4b1178c610bd4dccbac2905b9e0f3bbef4095279
Joe Greubel <joegreubel1@gmail.com>
2019-02-27 22:33:00 -0800

    WIP - Client and Auth Server interactions

--- auth-provider/src/main/java/com/jgreubel/oauthauthprovider/OauthAuthProviderApplication.java
+++ auth-provider/src/main/java/com/jgreubel/oauthauthprovider/OauthAuthProviderApplication.java
@@ -3,14 +3,27 @@ package com.jgreubel.oauthauthprovider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@RestController
 public class OauthAuthProviderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthAuthProviderApplication.class, args);
+	}
+
+	@RequestMapping({ "/user", "/me" })
+	public Map<String, String> user(Principal principal) {
+		Map<String, String> map = new LinkedHashMap<>();
+		map.put("name", principal.getName());
+		return map;
 	}
 
 }
--- client/src/main/java/com/jgreubel/oauthclient/OauthClientApplication.java
+++ client/src/main/java/com/jgreubel/oauthclient/OauthClientApplication.java
@@ -2,12 +2,45 @@ package com.jgreubel.oauthclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+//@EnableOAuth2Sso
+@RestController
 public class OauthClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthClientApplication.class, args);
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Configuration
+//	@Order
+	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			System.out.println("CONFIGURING STUFF");
+
+			http
+				.antMatcher("/**")
+					.authorizeRequests()
+					.antMatchers("/", "/login**", "/webjars/**", "/error**")
+						.permitAll()
+					.anyRequest()
+						.authenticated();
+		}
+
 	}
 
 }
